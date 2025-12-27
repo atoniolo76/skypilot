8ceb5936420761c352704f541f2e4c872ce9402b
Alessio Toniolo <atoniolo76@gmail.com>
2025-12-27 23:59:19 +0100

    create serve yaml for lambda and clean up launch_systems.sh

--- sky/lbbench/launch_systems.sh
+++ sky/lbbench/launch_systems.sh
@@ -4,10 +4,6 @@ if [ -z "$HF_TOKEN" ]; then
   echo "Error: HF_TOKEN is not set."
   exit 1
 fi
-if [ -z "$STABLE_BUILD_API" ]; then
-  echo "Error: STABLE_BUILD_API is not set."
-  exit 1
-fi
 
 names=()
 for i in {0..5}; do
@@ -15,7 +11,7 @@ for i in {0..5}; do
   names+=("$name")
 
   # Build the argument list safely with an array
-  cmd=( sky serve up examples/serve/external-lb/llm.yaml -y -n "$name" --env HF_TOKEN --env STABLE_BUILD_API --env REPLICAS --async )
+  cmd=( sky serve up examples/serve/external-lb/llm-lambda.yaml -y -n "$name" --env HF_TOKEN --env REPLICAS --async )
   (( i >= 4 )) && cmd+=( --env DO_PUSHING_ACROSS_LB=true --env LB_PUSHING_ENABLE_LB=false --env ENABLE_SELECTIVE_PUSHING=true )
   (( i == 4 )) && cmd+=( --env LB_POLICY=prefix_tree --env META_LB_POLICY=prefix_tree )
   (( i == 5 )) && cmd+=( --env LB_POLICY=consistent_hashing --env META_LB_POLICY=consistent_hashing )
